c1444127f737a00c89064b101fe2e31ba379786f
AlesciDavide <davi.alesci@Gmail.com>
2024-06-28 15:48:26 +0200

    fix vari

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/src/utilities/functions.php';
 
 checkUser($_GET["username"]?? 'logout', $_GET["password"]?? 'logout');
 
-
+$isOn = false;
 
 
 ?>
@@ -17,32 +17,39 @@ checkUser($_GET["username"]?? 'logout', $_GET["password"]?? 'logout');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./src/css/style.css">
+    <title>php-strong-password-generator</title>
 </head>
 <body>
-    <ul>
+    <ul class="myHeader">
         <li><a href="./index.php">Home</a></li>
         <li><a href="./src/login.php">Login</a></li>
-        
         <li><a href="./src/logout.php">Logout</a></li>
     </ul>
     
 
     <?php  foreach ($users as $user ) {?>
         <?php  if ($_SESSION["username"] == $user["username"] && $_SESSION["password"] == $user["password"]) {?>
+            <!-- Qua si dovranno inserire i contenuti della home page quando l'utente è loggato -->
             <h2>Benveuto: <?php echo ($_SESSION["username"]?? '') ?></h2>
             <h3>questi sono i tuoi contenuti dopo aver effettuato l'accesso:</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi repudiandae qui excepturi, dignissimos beatae tempore totam vitae atque, dolorum neque fugit velit cumque, labore autem at saepe soluta voluptates quaerat!</p>
+            <?php $isOn = true; ?>
             <?php break; ?>
         <?php }elseif($_SESSION["username"] == "logout" && $_SESSION["password"] == "logout" ){ ?>
+            <?php $isOn = false; ?>
         <?php break; ?>
         <?php }else{ ?>
-            <h2>Non sei registrato!! riinserisci i dati</h2>
-        <?php break; ?>
-
+            <?php $isOn = false; ?>
     <?php } ?>
     <?php } ?>
-
+            <section>
+                <?php if (!$isOn) {?>
+                    <!-- qua si potranno inserire i contenuti della homepage da visitatore -->
+                    <h2>Non sei loggato quindi i contenuti disponibili per te sono limitati</h2>
+            <p>registrati per vedere tutti i contenuti!!</p>
+                    <?php } ?>
+            </section>
 
 
 </body>
--- src/login.php
+++ src/login.php
@@ -5,7 +5,6 @@ session_start();
 
 
 
-var_dump($_SESSION);
 
 ?>
 
@@ -15,15 +14,19 @@ var_dump($_SESSION);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="../index.php" method="GET">
+    <section class="myForm">
+        <h1>Inserisci i tuoi dati!!</h1>
+        <form action="../index.php" method="GET">
         <label for="username"></label>
         <input type="text" name="username" id="username" placeholder="inserisci l'username">
         <label for="password"></label>
         <input type="text" name="password" id="password" placeholder="inserisci la Password">
         <button type="submit">send</button>
     </form>
+    </section>
 </body>
 </html>
